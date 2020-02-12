93f790a53101fff1a4bafd3dfeee493396cad335
Samat <iron.flex@yandex.ru>
2020-02-12 14:59:39 +0300

    Rectangle

---
+++ src/com/example/Rectangle.java
@@ -0,0 +1,30 @@
+package com.example;
+
+public class Rectangle {
+    /**
+     * 6 точек я беру как,2 клетки ,передаем количество клеток в параметор метода countRect.
+     * @param cell
+     * @return количество прямоугольников(квадратов)
+     */
+    private static int countRect(int cell) {
+        int ans = 0;
+
+        for (int length = 1; length <= Math.sqrt(cell);
+             ++length) {
+            for (int height = 0; height * length <= cell && length > height;
+                 ++height) {
+                ans++;
+                for (height = length; height * length <= cell;
+                     ++height) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(countRect(2));
+    }
+}
+
